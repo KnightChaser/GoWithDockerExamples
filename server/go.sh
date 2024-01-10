acb7d29a24e70669c1c7c97b30396fa7c76868e9
Lee Garam <47494005+KnightChaser@users.noreply.github.com>
2024-01-10 17:01:01 +0900

    A simple web application written in Go pkg

---
+++ server/go.sh
@@ -0,0 +1,11 @@
+#/bin/bash
+
+if [[ $(/usr/bin/id -u) -ne 0 ]]; then
+	echo "Not running as root"
+	exit
+fi
+
+echo "[+] Building the Dockerfile..."
+docker image build --tag goserver:0.1 .
+echo "[+] Running the service..."
+docker container run -t -p 9000:8080 goserver:0.1 
